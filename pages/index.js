6f8a998788ea408b446bfe52ad99b168283c7cb8
Alex Sirait <106827387+alexsirait@users.noreply.github.com>
2022-10-26 01:18:02 +0700

    wip

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="border-4 border-t-gray-900 border-r-sky-500 border-l-sky-500 border-b-gray-900">
         <div className="mb-5 w-32 h-32 shadows shadow-sky-500/20 bg-contain mx-auto p-1 rounded-full bg-[url(https://media-exp1.licdn.com/dms/image/C4D03AQGAezA47Gexhw/profile-displayphoto-shrink_800_800/0/1654084452104?e=1659571200&v=beta&t=1ddV5fLrSucG02MRcLGfUj581sbO4_RU-WoEgpJ_YcQ)]"></div>
       </div>
-      <h1 className='text-white text-2xl font-semibold tracking-tighter mb-5'>Alex Sirait</h1>
+      <h1 className='text-white text-2xl font-semibold tracking-tighter mb-5'>Alex Sabeth with Love </h1>
       <div className="text-white leading-loose text-md md:text-lg">
         <p className="mb-5">I am a <span className="underline underline-offset-1 decoration-dotted">Creative Full-Stack Web Developer</span> and an Informatics Engineering Student, who loves building things, solve problems and learn new things. 🚀</p>
         <a
